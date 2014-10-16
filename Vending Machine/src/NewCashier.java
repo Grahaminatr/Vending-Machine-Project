2d878eb39d4debe80790dc66f726819ab050b5f2
alexander.whitfield <alexander.whitfield@MU-C3-23.mullen.pvt.k12.co.us>
2014-10-16 14:18:07 -0600

    New Commit

---
+++ Vending Machine/src/NewCashier.java
@@ -0,0 +1,13 @@
+
+public class NewCashier extends java.lang.Object
+	{
+	private int currentCents;
+	private boolean enterMoney=true;
+	public int[] addMoney()
+		{
+		while(enterMoney)
+			{
+			
+			}
+		}
+	}
